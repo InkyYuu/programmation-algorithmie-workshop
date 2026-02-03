ea358e2326c4b68775fa9ddcbf7b6760c0b79cdc
Kellian Bredeau <bredeaukellian@gmail.com>
2026-02-03 12:41:26 +0100

    Jour 2
    - Disque
    - Cercle
    - Animation
    - Rosace
    - Mosaïque
    - Mosaïque miroir
    - Glitch
    - Tri de pixels

--- README.md
+++ README.md
@@ -50,3 +50,55 @@
 <div style="background-color: #fff3cd; border-left: 4px solid #ffc107; padding: 10px; margin: 10px 0; border-radius: 5px; font-family: Arial, sans-serif; color: #856404;">
     💡 Il est possible de modifier la luminosité en changeant la valeur du paramètre <strong>mode</strong>, par exemple <strong>brightness(img, Brightness::Darker)</strong> pour une image plus sombre <i>(par défaut, l'image est éclaircie)</i>.
 </div>
+
+### ✔ Disque
+
+![Disk](output/disk.png)
+
+<div style="background-color: #fff3cd; border-left: 4px solid #ffc107; padding: 10px; margin: 10px 0; border-radius: 5px; font-family: Arial, sans-serif; color: #856404;">
+    💡 Il est possible de modifier le rayon et la position du disque en changeant les paramètres de la fonction <strong>disk</strong>, par exemple <strong>disk(img, 150.f, 250, 250)</strong> pour un disque de rayon 150 centré en (250, 250) <i>(par défaut, le rayon est de 100 et le disque est centré)</i>.
+</div>
+
+### ✔ Cercle
+
+![Circle](output/circle.png)
+
+<div style="background-color: #fff3cd; border-left: 4px solid #ffc107; padding: 10px; margin: 10px 0; border-radius: 5px; font-family: Arial, sans-serif; color: #856404;">
+    💡 Il est possible de modifier le rayon, l'épaisseur et la position du cercle en changeant les paramètres de la fonction <strong>circle</strong>, par exemple <strong>circle(img, 150.f, 5.f, 250, 250)</strong> pour un cercle de rayon 150, d'épaisseur 5 centré en (250, 250) <i>(par défaut, le rayon est de 100, l'épaisseur est de 3 et le cercle est centré)</i>.
+</div>
+
+### ✔ Animation
+
+![Animation](output/animation.gif)
+
+<div style="background-color: #fff3cd; border-left: 4px solid #ffc107; padding: 10px; margin: 10px 0; border-radius: 5px; font-family: Arial, sans-serif; color: #856404;">
+    💡 Il est possible de modifier la coordonnée Y, la durée et le nombre d'images par seconde de l'animation en changeant les paramètres de la fonction <strong>animation</strong>, par exemple <strong>animation(5, 5, 30)</strong> pour une animation de 5 secondes à 30 images par seconde avec un cercle à Y 5 <i>(par défaut, la durée est de 3 secondes à 25 images par seconde avec un cercle centré verticalement)</i>.
+</div>
+
+### ✔ Rosace
+
+![Rosette](output/rosette.png)
+
+<div style="background-color: #fff3cd; border-left: 4px solid #ffc107; padding: 10px; margin: 10px 0; border-radius: 5px; font-family: Arial, sans-serif; color: #856404;">
+    💡 Il est possible de modifier le nombre de branches de la rosace, l'épaisseur des cercles et leur rayon en changeant les paramètres de la fonction <strong>rosette</strong>, par exemple <strong>rosette(img, 12, 1, 150)</strong> pour une rosace à 12 branches avec des cercles d'épaisseur 1 et de rayon 150 <i>(par défaut, la rosace a 6 branches avec des cercles d'épaisseur 0.5 et de rayon 100)</i>.
+</div>
+
+### ✔ Mosaïque
+
+![Mosaic](output/mosaic.png)
+
+<div style="background-color: #fff3cd; border-left: 4px solid #ffc107; padding: 10px; margin: 10px 0; border-radius: 5px; font-family: Arial, sans-serif; color: #856404;">
+    💡 Il est possible de modifier le nombre de tuiles (horizontalement et verticalement) en changeant le paramètre de la fonction <strong>mosaic</strong>, par exemple <strong>mosaic(img, 10)</strong> pour une mosaïque avec 10 tuiles sur chaque ligne et sur chaque colonne <i>(par défaut, le nombre de 5)</i>.
+</div>
+
+### ✔ Mosaïque miroir
+
+![Mosaic Mirror](output/mosaic_mirror.png)
+
+### ✔ Glitch
+
+![Glitch](output/glitch.png)
+
+### ✔ Tri de pixels
+
+![Pixel Sort](output/pixel_sort.png)
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <algorithm>
+#include <numbers>
 
 /**
  * Conserve uniquement la composante verte de chaque pixel de l'image.
@@ -164,7 +166,7 @@ void rotate90(sil::Image& img)
         }
     }
 
-    img = std::move(rotated_image); // On remplace l'ancienne image par la nouvelle
+    img = std::move(rotated_image); // On remplace l'ancienne image par la nouvelle équivaut à img.pixels() = new_img.pixels();
 }
 
 /**
@@ -221,6 +223,257 @@ void brightness(sil::Image& img, Brightness mode)
     }
 }
 
+/**
+ * Dessine un disque blanc sur un fond noir.
+ * Le rayon et le centre du disque peuvent être spécifiés.
+ * Si le centre n'est pas spécifié, il est placé au centre de l'image.
+ *
+ * @param img Image à modifier (type sil::Image), modifiée en place.
+ * @param radius Rayon du disque (par défaut 100.f).
+ * @param centerX Coordonnée x du centre du disque (par défaut -1, ce qui signifie centré horizontalement).
+ * @param centerY Coordonnée y du centre du disque (par défaut -1, ce qui signifie centré verticalement).
+ */
+void disk(sil::Image& img, float radius = 100.f, int centerX = -1, int centerY = -1)
+{
+    int width = img.width();
+    int height = img.height();
+    if (centerX == -1) centerX = width / 2;
+    if (centerY == -1) centerY = height / 2;
+
+    for (int y{0}; y < height; y++)
+    {
+        for (int x{0}; x < width; x++)
+        {
+
+            // Formule de la distance entre deux points : d = sqrt((x2 - x1)² + (y2 - y1)²)
+            float dx = x - centerX; // x2 - x1
+            float dy = y - centerY; // y2 - y1
+            float distance = std::sqrt(dx * dx + dy * dy); // sqrt((x2 - x1)² + (y2 - y1)²
+            if (distance < radius)
+            {
+                img.pixel(x, y) = glm::vec3{1.f, 1.f, 1.f};
+            }
+        }
+    }
+}
+
+/**
+ * Dessine un cercle blanc sur un fond noir.
+ * Le rayon, l'épaisseur et le centre du cercle peuvent être spécifiés.
+ * Si le centre n'est pas spécifié, il est placé au centre de l'image.
+ *
+ * @param img Image à modifier (type sil::Image), modifiée en place.
+ * @param radius Rayon du cercle (par défaut 100.f).
+ * @param thickness Épaisseur du cercle (par défaut 3.f).
+ * @param centerX Coordonnée x du centre du cercle (par défaut -1, ce qui signifie centré horizontalement).
+ * @param centerY Coordonnée y du centre du cercle (par défaut -1, ce qui signifie centré verticalement).
+ */
+void circle(sil::Image& img, float radius = 100.f, float thickness = 3.f, int centerX = -1, int centerY = -1)
+{
+    int width = img.width();
+    int height = img.height();
+    if (centerX == -1) centerX = width / 2;
+    if (centerY == -1) centerY = height / 2;
+
+    for (int y{0}; y < height; y++)
+    {
+        for (int x{0}; x < width; x++)
+        {
+            // Formule de la distance entre deux points : d = sqrt((x2 - x1)² + (y2 - y1)²)
+            float dx = x - centerX; // x2 - x1
+            float dy = y - centerY; // y2 - y1
+            float distance = std::sqrt(dx * dx + dy * dy); // sqrt((x2 - x1)² + (y2 - y1)²
+            if (distance < radius + thickness && distance > radius - thickness)
+            {
+                img.pixel(x, y) = glm::vec3{1.f, 1.f, 1.f};
+            }
+        }
+    }
+}
+
+/**
+ * Crée une animation en dessinant un disque blanc se déplaçant horizontalement sur un fond noir.
+ * Le disque se déplace de la gauche vers la droite de l'image.
+ * Le nombre de secondes et le nombre d'images par seconde (ips) peuvent être spécifiés.
+ * Le centre vertical du disque peut également être spécifié.
+ *
+ * @param centerY Coordonnée y du centre du disque (par défaut -1, ce qui signifie centré verticalement).
+ * @param seconds Durée de l'animation en secondes (par défaut 3 secondes).
+ * @param ips Images par seconde (par défaut 25 ips).
+ */
+void animation(int centerY = -1, int seconds = 3, int ips = 25)
+{
+    const int width = 500;
+    const int height = 500;
+
+    if (centerY == -1) centerY = height / 2;
+
+    for (int x{0}; x < width; x += width / (seconds * ips))
+    {
+        sil::Image img{width, height};
+        disk(img, 100.f, x, centerY);
+        img.save("output/animation/frame_" + std::to_string(x) + ".png");
+    }
+}
+
+/**
+ * Dessine une rosace composée de plusieurs cercles blancs sur un fond noir.
+ * Le nombre de cercles, la "serréité" (tightness) et le rayon des cercles peuvent être spécifiés.
+ * Les cercles sont disposés de manière circulaire autour du centre de l'image.
+ *
+ * @param img Image à modifier (type sil::Image), modifiée en place.
+ * @param circles Nombre de cercles dans la rosette (par défaut 6).
+ * @param tightness Facteur de serréité des cercles (par défaut 0.5f).
+ * @param radius Rayon des cercles (par défaut 100.f).
+ */
+void rosette(sil::Image& img, int circles = 6, float tightness = 0.5f, float radius = 100.f)
+{
+    int width = img.width();
+    int height = img.height();
+    float centerX = width / 2.f;
+    float centerY = height / 2.f;
+
+    float offset = radius * 2 * tightness;
+
+    for (int i = 0; i < circles; ++i)
+    {
+        // Calcul de l'angle actuel en radians (formule : angle = (2 * π * i) / nombre_de_cercles)
+        float angle = (2.0f * std::numbers::pi * i) / circles;
+
+        // Conversion coordonnées polaires en coordonnées cartésiennes (formules : x = centerX + offset * cos(angle) et y = centerY + offset * sin(angle))
+        float cx = centerX + offset * std::cos(angle);
+        float cy = centerY + offset * std::sin(angle);
+
+        circle(img, radius, 3.f, static_cast<int>(cx), static_cast<int>(cy));
+    }
+
+    // Cercle central
+    circle(img, radius, 3.f, static_cast<int>(centerX), static_cast<int>(centerY));
+}
+
+/**
+ * Applique un effet de mosaïque à l'image en répétant l'image plusieurs fois.
+ *
+ * @param img Image à modifier (type sil::Image), modifiée en place.
+ * @param copies Nombre de copies de l'image (par défaut 5).
+ */
+void mosaic(sil::Image& img, int copies = 5)
+{
+    int width = img.width();
+    int height = img.height();
+    sil::Image mosaic_image{width*5, height*5};
+
+    for (int y{0}; y < height*5; y++)
+    {
+        for (int x{0}; x < width*5; x++)
+        {
+            mosaic_image.pixel(x, y) = img.pixel(x % width, y % height);
+        }
+    }
+
+    img = std::move(mosaic_image);
+}
+
+/**
+ * Applique un effet de mosaïque avec miroir à l'image en répétant l'image plusieurs fois et en inversant alternativement les lignes.
+ *
+ * @param img Image à modifier (type sil::Image), modifiée en place.
+ */
+void mosaic_mirror(sil::Image& img)
+{
+    int width = img.width();
+    int height = img.height();
+    sil::Image mosaic_image{width*5, height*5};
+
+    for (int y{0}; y < height*5; y++)
+    {
+        for (int x{0}; x < width*5; x++)
+        {
+            // Détermination de la tuile courante
+            int tile_x = x / width;
+            int tile_y = y / height;
+
+            // Coordonnées dans l'image source
+            int src_x = x % width;
+            int src_y = y % height;
+
+            // Effet de miroir horizontal sur les colonnes impaires
+            if (tile_x % 2 == 1)
+                src_x = width - 1 - src_x;
+
+            // Effet de miroir vertical sur les lignes impaires
+            if (tile_y % 2 == 1)
+                src_y = height - 1 - src_y;
+
+            mosaic_image.pixel(x, y) = img.pixel(src_x, src_y);
+        }
+    }
+
+    img = std::move(mosaic_image);
+}
+
+/**
+ * Applique un effet de glitch à l'image en déplaçant aléatoirement des blocs de pixels.
+ *
+ * @param img Image à modifier (type sil::Image), modifiée en place.
+ */
+void glitch(sil::Image& img)
+{
+    int width = img.width();
+    int height = img.height();
+
+    for (int x = 0; x < width; x ++)
+    {
+        for (int y = 0; y < height; y ++)
+        {
+            const int random = random_int(0, 1500);
+            if(random == 0) {
+                int rectWidth = random_int(10, 50);
+                int rectHeight = random_int(1, 10);
+                int x2 = random_int(0, width - rectWidth);
+                int y2 = random_int(0, height - rectHeight);
+
+                for (int dx = 0; dx < rectWidth; dx++) {
+                    for (int dy = 0; dy < rectHeight; dy++) {
+                        if (x + dx < width && y + dy < height && x2 + dx < width && y2 + dy < height) {
+                            std::swap(img.pixel(x + dx, y + dy), img.pixel(x2 + dx, y2 + dy));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Trie les pixels de l'image par segments aléatoires en fonction de leur luminosité totale.
+ * Parcourt les pixels de l'image et, avec une probabilité de 1 sur 75, sélectionne un segment de pixels de longueur aléatoire entre 20 et 75 pixels.
+ * Trie ensuite ce segment de pixels en fonction de la somme de leurs composantes R, G et B (luminosité totale).
+ * 
+ * @param img Image à modifier (type sil::Image), modifiée en place.
+ */
+void pixelSort(sil::Image& img)
+{
+    std::vector<glm::vec3> pixels = img.pixels();
+    int pixelIndex = 0;
+    while (pixelIndex < img.width() * img.height())
+    {
+        int random = random_int(0, 75);
+        if (random == 0) {
+            int randomLength = random_int(20, 75);
+            int endIndex = std::min(pixelIndex + randomLength, img.width() * img.height());
+            std::sort(pixels.begin() + pixelIndex, pixels.begin() + endIndex, [](const glm::vec3& a, const glm::vec3& b) {
+                return (a.r + a.g + a.b) < (b.r + b.g + b.b); // Tri par luminosité totale (addition des composantes R, G et B)
+            });
+            pixelIndex = endIndex;
+        }
+        else {
+            pixelIndex++;
+        }
+    }
+    img.pixels() = pixels;
+}
+
 int main()
 {
     sil::Image image{"images/logo.png"};
@@ -266,4 +519,36 @@ int main()
     image = sil::Image{"images/photo.jpg"};
     brightness(image, Brightness::Brighter);
     image.save("output/brighter.png");
+
+    image = sil::Image{500, 500};
+    disk(image);
+    image.save("output/disk.png");
+
+    image = sil::Image{500, 500};
+    circle(image);
+    image.save("output/circle.png");
+
+    animation();
+
+    image = sil::Image{500, 500};
+    rosette(image);
+    image.save("output/rosette.png");
+
+    image = sil::Image{"images/logo.png"};
+    mosaic(image);
+    image.save("output/mosaic.png");
+
+    image = sil::Image{"images/logo.png"};
+    mosaic_mirror(image);
+    image.save("output/mosaic_mirror.png");
+
+    image = sil::Image{"images/logo.png"};
+    glitch(image);
+    image.save("output/glitch.png");
+
+    image = sil::Image{"images/logo.png"};
+    pixelSort(image);
+    image.save("output/pixel_sort.png");
+
+    return 0;
 }
